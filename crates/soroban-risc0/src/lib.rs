b5634c029b1d41f91c0ef9876481e3b86ac11a3c
Brian Anderson <andersrb@gmail.com>
2023-09-22 19:06:52 -0600

    .

--- crates/soroban-risc0/src/lib.rs
+++ crates/soroban-risc0/src/lib.rs
@@ -4,6 +4,9 @@
 use soroban_sdk::*;
 use risc0_zkvm::Receipt;
 
+extern crate alloc;
+use alloc::vec::Vec as RustVec;
+
 #[contract]
 struct Verifier;
 
@@ -11,8 +14,12 @@ struct Verifier;
 impl Verifier {
     pub fn verify(
         env: Env,
+        image_id: BytesN<32>,
         receipt: ReceiptBundle,
     ) -> bool {
+        let receipt = receipt.to_receipt();
+        //let res = receipt.verify(&image_id);
+
         todo!()
     }
 }
@@ -28,4 +35,10 @@ pub struct SuccinctReceipt {
     pub seal: Vec<u32>,
     pub control_id: BytesN<32>,
     pub meta: Vec<u32>,
+}
+
+impl ReceiptBundle {
+    fn to_receipt(&self) -> Receipt {
+        todo!()
+    }
 }
