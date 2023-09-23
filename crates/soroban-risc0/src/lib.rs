8129866a924ee7d87f8bf6e8758cb889bbc6c270
Brian Anderson <andersrb@gmail.com>
2023-09-22 19:03:04 -0600

    .

--- crates/soroban-risc0/src/lib.rs
+++ crates/soroban-risc0/src/lib.rs
@@ -1,6 +1,8 @@
+#![allow(unused)]
 #![no_std]
 
 use soroban_sdk::*;
+use risc0_zkvm::Receipt;
 
 #[contract]
 struct Verifier;
@@ -9,8 +11,21 @@ struct Verifier;
 impl Verifier {
     pub fn verify(
         env: Env,
-        bundle: Bytes,
+        receipt: ReceiptBundle,
     ) -> bool {
         todo!()
     }
+}
+
+#[contracttype]
+pub struct ReceiptBundle {
+    pub receipt: SuccinctReceipt,
+    pub journal: Bytes,
+}
+
+#[contracttype]
+pub struct SuccinctReceipt {
+    pub seal: Vec<u32>,
+    pub control_id: BytesN<32>,
+    pub meta: Vec<u32>,
 }
